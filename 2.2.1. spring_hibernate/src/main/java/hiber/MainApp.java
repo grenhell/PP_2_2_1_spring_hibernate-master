56ecf03fd6bdd65c7b8e1fa7d1cddbd4fd1f830c
Denis Doich <doich225@gmail.com>
2022-07-26 20:43:22 +0300

    Initial commit

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -0,0 +1,41 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    UserService userService = context.getBean(UserService.class);
+      Car car1 = new Car("car1", 12);
+      Car car2 = new Car("car2", 23);
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru", car1);
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru", car2);
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+      userService.addcar(car1);
+      userService.addcar(car2);
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
+
+     System.out.println(userService.getBycar("car1", 12).toString());
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+          System.out.println(user.toString());
+      }
+
+      context.close();
+   }
+}
---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,43 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   public void addcar(Car car) { sessionFactory.getCurrentSession().save(car);}
+
+   @Override
+   public User getBycar(String model, int serial) {
+      String hql = "SELECT u FROM User u WHERE u.car.series = :car_Serial AND u.car.model = :model";
+      Query query = sessionFactory.getCurrentSession().createQuery(hql);
+      query.setParameter("car_Serial", serial);
+      query.setParameter("model", model);
+      return (User) query.getSingleResult();
+   }
+
+}
